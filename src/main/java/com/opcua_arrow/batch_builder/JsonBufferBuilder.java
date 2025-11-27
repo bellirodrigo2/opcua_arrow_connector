9c280cf280304f29b67bbe404c98b5328fb19bd9
RBELLI <bellirodrigo2@gmail.com>
2025-11-27 19:40:44 -0300

    add json encoding

--- src/main/java/com/opcua_arrow/batch_builder/JsonBufferBuilder.java
+++ src/main/java/com/opcua_arrow/batch_builder/JsonBufferBuilder.java
@@ -2,24 +2,42 @@ package com.opcua_arrow.batch_builder;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opcua_arrow.data.TSValue;
 
 public class JsonBufferBuilder implements IBufferBuilder {
+
+    private ObjectMapper mapper = new ObjectMapper();
+    private List<TSValue> buffer;
+
     @Override
     public void appendList(List<TSValue> dataValues) {
-        // TODO Auto-generated method stub
 
+        buffer.addAll(dataValues);
     }
 
     @Override
     public byte[] flush() {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            // Extrai apenas os values
+            List<Object> values = buffer.stream()
+                    .map(ts -> ts.value)
+                    .toList();
+
+            // Converte para JSON e retorna como byte[]
+            return mapper.writeValueAsBytes(values);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao serializar buffer para JSON", e);
+        } finally {
+            buffer.clear(); // importante: limpa o buffer
+        }
+
     }
 
     @Override
     public void close() {
-        // TODO Auto-generated method stub
+        buffer.clear();
 
     }
 }
--- src/main/java/com/opcua_arrow/opcua/milo/TSValueAlarmFactory.java
+++ src/main/java/com/opcua_arrow/opcua/milo/TSValueAlarmFactory.java
@@ -6,10 +6,10 @@ import com.opcua_arrow.data.DataPoint;
 import com.opcua_arrow.data.TSValue;
 
 public class TSValueAlarmFactory {
-    static TSValue createTSValue(DataPoint dp, String json) {
+    static TSValue createTSValue(DataPoint dp, String alarmJsonString) {
         long timestamp = System.nanoTime();
         Map<String, String> map = Map.of("name", dp.getName(), "description", dp.getDescription(), "nodeId",
-                dp.getNodeId(), "pointId", String.valueOf(dp.getPointId()), "content", json);
+                dp.getNodeId(), "pointId", String.valueOf(dp.getPointId()), "alarm", alarmJsonString);
 
         return new TSValue(dp.getPointId(), timestamp, (Object) map, true, dp.getWriteGroup());
 
